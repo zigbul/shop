160de34316fd7f4b95fe4a6b1be4dc3347c898d3
Zigbul <parfenov.georgiy@bk.ru>
2025-06-11 18:38:14 +0300

    add product model

--- shop/Controllers/HomeController.cs
+++ shop/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
 using shop.Models;
 
 namespace shop.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "products.json");
+            var json = System.IO.File.ReadAllText(filePath);
+            var products = JsonSerializer.Deserialize<List<Product>>(json);
+
+            var result = "";
+
+            foreach (var product in products)
+            {
+                result += $"ID: {product.Id}\n" +
+                          $"Name: {product.Name}\n" +
+                          $"Cost: {product.Cost}\n\n";
+            }
+
+            return result;
         }
 
         public IActionResult Privacy()
