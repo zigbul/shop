ed700be92bb931af66e3e5e6bf400b7b96587779
Zigbul <parfenov.georgiy@bk.ru>
2025-06-12 02:16:48 +0300

    add cart and position models

---
+++ shop/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Cart
+    {
+        public List<Position> Positions;
+
+        public Cart() 
+        {
+            Positions = new List<Position>();
+        }
+    }
+}
---
+++ shop/Models/Position.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Position
+    {
+        public Product product { get; set; }
+
+        public int Count { get; set; }
+
+        public int Cost { get; set; }
+    }
+}
