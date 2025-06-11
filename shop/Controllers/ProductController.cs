2d01af606c21ead0bf8a9dbcbf0bd1ee0ae4e27e
Zigbul <parfenov.georgiy@bk.ru>
2025-06-12 02:02:54 +0300

    add and style product view

--- shop/Controllers/ProductController.cs
+++ shop/Controllers/ProductController.cs
@@ -13,16 +13,11 @@ namespace OnlineShopWebApp.Controllers
             _productsStorage = new ProductsStorage();
         }
 
-        public string Index(int id = 1)
+        public IActionResult Index(int id = 1)
         {
             var product = _productsStorage.TryGetById(id);
 
-            if (product == null)
-            {
-                return $"Продукта с Id = {id} не существует";
-            }
-
-            return product.ToString();
+            return View(product);
         }
     }
 }
