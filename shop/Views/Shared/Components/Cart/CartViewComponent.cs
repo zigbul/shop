001fa24e579629419168e810718972cafc5e085c
Zigbul <parfenov.georgiy@bk.ru>
2025-06-14 00:45:42 +0300

    add cart component

---
+++ shop/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartsStorage _cartsStorage;
+
+        public CartViewComponent(ICartsStorage cartsStorage)
+        {
+            _cartsStorage = cartsStorage;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cart = _cartsStorage.TryGetCardByUserId("1");
+            var allItemsAmount = cart?.ItemsAmount;
+
+            return View("Cart", allItemsAmount);
+        }
+    }
+}
