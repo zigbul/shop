002dc880405f8fd31155f0151325d76436157f2a
Zigbul <parfenov.georgiy@bk.ru>
2025-06-14 18:27:38 +0300

    add AdminProducts component

---
+++ shop/Views/Shared/Components/AdminProducts/AdminProductsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Services;
+
+namespace OnlineShopWebApp.Views.Shared.Components.AdminProducts
+{
+    public class AdminProductsViewComponent : ViewComponent
+    {
+        private readonly IProductsStorage _productsStorage;
+
+        public AdminProductsViewComponent(IProductsStorage productsStorage)
+        {
+            _productsStorage = productsStorage;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("AdminProducts");
+        }
+    }
+}
