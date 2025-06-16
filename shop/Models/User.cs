635ef20ee3aaba7e4ddf8ff5533a8d725621bcaf
Zigbul <parfenov.georgiy@bk.ru>
2025-06-16 22:17:24 +0300

    add User model with data notations

---
+++ shop/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        public required Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Не указан логин")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Логин должен быть от 3ёх до 10 символов")]
+        public required string Login { get; set; }
+
+        [Required(ErrorMessage = "Укажите ваш email")]
+        [EmailAddress(ErrorMessage = "Введите валидный email")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Укажите пароль")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
