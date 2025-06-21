89306bf233d5436424dafe3b5e94edc1da758ce4
Zigbul <parfenov.georgiy@bk.ru>
2025-06-21 17:22:10 +0300

    add roles service

---
+++ shop/Services/IRolesStorage.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Services
+{
+    public interface IRolesStorage
+    {
+        List<Role> GetAll();
+
+        Role? GetByName(string name);
+
+        void Remove(Role role);
+
+        void Add(Role role);
+    }
+}
